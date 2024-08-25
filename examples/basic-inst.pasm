37e1c82a9393bff577e7b875d17434db759d93c4
Mayuge <mayuge.studio.public@gmail.com>
2024-08-26 01:00:29 +0900

    Enable to read file from cmd arguments

---
+++ examples/basic-inst.pasm
@@ -0,0 +1,20 @@
+mov acc, 34
+add acc, 35
+dump acc
+mov acc, 0
+
+mov acc, 150
+sub acc, 50
+dump acc
+mov acc, 0
+
+mov acc, 25
+mul acc, 2
+dump acc
+mov acc, 0
+
+mov acc, 14
+div acc, 2
+dump acc
+mov acc, 0
+
--- main.go
+++ main.go
@@ -8,15 +8,21 @@ import (
 )
 
 func main() {
-	reset := "mov acc, 0\n"
-	dump := "dump acc\n"
-	source := ""
-	source += "mov acc, 34\nadd acc, 35\n" + dump + reset
-	source += "mov acc, 150\nsub acc, 50\n" + dump + reset
-	source += "mov acc, 25\nmul acc, 2\n" + dump + reset
-	source += "mov acc, 14\ndiv acc, 2\n" + dump + reset
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "No input file was provided")
+		os.Exit(1)
+	}
+	
+	filepath := args[1]
 
-	fmt.Printf("raw source:\n%s\n", source)
+	bytes, err := os.ReadFile(filepath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: could not open file `%s: %w\n", filepath, err)
+	}
+
+	source := string(bytes)
+
 	tokens, err := asm.LexProgram(source)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR: In `TokenizeSource`", err)
