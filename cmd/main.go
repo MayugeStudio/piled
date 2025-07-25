c4038d1e0e10344772bec63d4c7d43ee469f84a4
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-07-26 01:01:27 +0900

    enable to load program from file

---
+++ cmd/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"piled"
+)
+
+func runREPL() {
+	s := bufio.NewScanner(os.Stdin)
+cmd:
+	for {
+		fmt.Print("piled> ")
+		if !s.Scan() {
+			break
+		}
+		prompt := s.Text()
+		switch prompt {
+		case "exit":
+			{
+				fmt.Println("bye!")
+				break cmd
+			}
+		default:
+			{
+				runningErr := piled.RunSource(prompt);
+				if runningErr != nil {
+					fmt.Fprintf(os.Stderr, "Error: %s\n", runningErr)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	argv := os.Args
+	if len(argv) == 1 {
+		runREPL()
+	} else {
+		_ = argv[0]
+		argv = argv[1:]
+		filename := argv[0]
+		source, readErr := piled.ReadSourceFromFile(filename)
+		if readErr != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", readErr)
+			os.Exit(1)
+		}
+		runningErr := piled.RunSource(source)
+		if runningErr != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", runningErr)
+			os.Exit(1)
+		}
+	}
+}
---
+++ piled.go
@@ -0,0 +1,41 @@
+package piled
+
+import (
+	"os"
+	"piled/scanner"
+	"piled/parser"
+	"piled/compiler"
+	"piled/runtime"
+)
+
+func RunSource(source string) error {
+	tokens, scanErr := scanner.ScanProgram(source)
+	if scanErr != nil {
+		return scanErr
+	}
+
+	p := parser.New(tokens)
+	exprs, parseErr := p.ParseExpr()
+	if parseErr != nil {
+		return parseErr
+	}
+
+	c := compiler.New()
+	codes, compileErr := c.Compile(exprs)
+	if compileErr != nil {
+		return compileErr
+	}
+
+	vm := runtime.NewVM(codes)
+
+	vm.Run()
+	return nil
+}
+
+func ReadSourceFromFile(filename string) (string, error) {
+	source, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(source), nil
+}
