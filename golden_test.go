ac198b162d7747ab9ee896e626c443ec0e666c25
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-08-06 20:21:17 +0900

    deal with crlf issue

--- golden_test.go
+++ golden_test.go
@@ -18,22 +18,25 @@ func TestGoldenFiles(t *testing.T) {
 
 	for _, inputfile := range files {
 		name := strings.TrimSuffix(filepath.Base(inputfile), ".pd")
-		wantPath := filepath.Join(testDir, name+".stdout.golden")
+		wantPath := filepath.Join(testDir, name+".stdout")
 		binaryfile := filepath.Join(testDir, name+".pdb")
 
 		t.Run(name, func(t *testing.T) {
 			// compile piled into .pdb
 			cmd := exec.Command("./piled.exe", "compile", inputfile)
-			err := cmd.Run()
 			var out bytes.Buffer
 			cmd.Stdout = &out
 			cmd.Stderr = &out
+
+			err := cmd.Run()
 			if err != nil {
 				t.Fatalf("failed to compile piled executable: %v\n%s", err, out.String())
 			}
 
 			// run .pdb file
 			cmd = exec.Command("./piled.exe", "run", binaryfile)
+			cmd.Stdout = &out
+			cmd.Stderr = &out
 			out.Reset()
 
 			err = cmd.Run()
@@ -41,17 +44,23 @@ func TestGoldenFiles(t *testing.T) {
 				t.Fatalf("failed to run piled executable: %v\n%s", err, out.String())
 			}
 
-			got := out.String()
+			got := strings.ReplaceAll(out.String(), "\r\n", "\n")
 
 			wantBytes, err := os.ReadFile(wantPath)
-			want := string(wantBytes)
+			if err != nil {
+				t.Fatalf("failed to read expected-output-file: %v", err)
+			}
+
+			want := strings.ReplaceAll(string(wantBytes), "\r\n", "\n")
 
 			if got != want {
 				t.Errorf("output does not match golden file\n")
 				t.Errorf("===== Got =====\n")
-				t.Errorf("%s\n", got)
+				t.Errorf("%#v\n", got)
+				t.Errorf("len: %d\n", len(got))
 				t.Errorf("===== Want =====\n")
-				t.Errorf("%s\n", want)
+				t.Errorf("%#v\n", want)
+				t.Errorf("len: %d\n", len(want))
 			}
 		})
 	}
